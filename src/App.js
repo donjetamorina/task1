45325e6f38f012bb5e8def636f0a7540d68b3a5a
donjetamorina <donjetam33@gmail.com>
2022-03-03 23:16:05 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Data1, Data2 } from "./first-task/date";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './first-task/first.scss'
+import './first-task/second.scss'
+
+  function App()  {
+  return  (
+       
+ <div className="All">
+      <div className="first-task">
+          {Data1.map((props) => {
+            return(
+              <div className="item1">
+                <div className="icon">{props.icon}</div>
+                <div className="block">
+                  <h6>{props.tittle}</h6>
+                  <p>{props.paragraf}</p>
+                </div>
+            </div>
+            )
+          })}
+      </div>
+      
+      <div className="second-cards">
+        {Data2.map((props) => {
+          return(
+            <div className="card1">
+                <div className="info">
+                    <div className="date">{props.number}</div>
+                    <div className="inline">
+                      <div className="category" style={{background: `${props.color}`}}>{props.button}</div>
+                      <div className="profile"></div>
+                      <span>Kevin Joe</span>
+                    </div>
+                    <p>{props.text}</p>
+                  </div>
+
+                  <button>Download</button>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
+  
 }
+
 
 export default App;
---
+++ src/first-task/date.js
@@ -0,0 +1,112 @@
+import {MdDomain} from 'react-icons/md'
+import {FaRuler, FaMedal} from 'react-icons/fa'
+import {AiOutlineGlobal, AiOutlineSearch, AiOutlinePercentage, AiOutlineBulb} from 'react-icons/ai'
+import {HiOutlineSpeakerphone} from 'react-icons/hi'
+import {BiText} from 'react-icons/bi'
+
+export const Data1 = [
+    {
+        icon:<MdDomain />,
+        tittle : 'The TLD',
+        paragraf: 'Dose the domain extansion math the language of the domain name?' 
+    },
+    {
+        icon:<FaRuler />,
+        tittle :'Domain Length',
+        paragraf: 'Is the domain short enough to remember?' 
+    },
+    {
+        icon:<AiOutlineGlobal />,
+        tittle:'Language' ,
+        paragraf: 'How complex is the actull domain name?'
+    },
+    {
+        icon:<FaMedal />,
+        tittle :'International recognition', 
+        paragraf: 'Can the domain name be used on an international scale?'
+    },
+    {
+        icon:<AiOutlineSearch />,
+        tittle : 'Search engine' ,
+        paragraf: 'Dose the domain follow search engine guidelines?'
+    },
+    {
+        icon:<HiOutlineSpeakerphone />,
+        tittle:'Advertising Potencial ',
+        paragraf:'Could the domain be used for advertising campaings?'  
+    },
+    {
+        icon:<AiOutlinePercentage />,
+        tittle: 'Sales Opportunities', 
+        paragraf: 'Can the domain name be used on an internetanola scale?'
+    },
+    {
+        icon:<BiText />,
+        tittle : 'Typo susceptibility',
+        paragraf:'How hight is the risk of mistyping the donaim name?' 
+    },
+    {
+        icon:<AiOutlineBulb />,
+        tittle : 'Business potential' ,
+        paragraf:'Can the domain be used as you company address?'
+    }
+]
+
+export const Data2 = [
+    {
+        color:'green',
+        number:  '3.3.0 (12/12/2021)' ,
+        button:'New',
+        text:'Mac or Windows PC. WhatsApp must be installed on your phone. By clicking the Download button, you agree to our Terms and Privacy Policy.'
+    },
+    {
+        color:'blue',
+        number :' 2.9.3 (02/05/2020)',
+        button:'Fix',
+        text :'Google Drive or backup to Google Photos, and access all of your content directly from your PC or Mac.Go to Google Drive Download Drive for desktop.'
+    },
+    {
+        color:'purple',
+        number :'2.2.0 (01/11/2019)' ,
+        button:'Improvement',
+        text: 'You can download a copy of the data saved  in your Google Account to use it with another account or service, or to keep for your records.'  
+    },
+    {
+        color:'green',
+        number:  '3.3.0 (12/12/2021)' ,
+        button:'New',
+        text:'Mac or Windows PC. WhatsApp must be installed on your phone. By clicking the Download button, you agree to our Terms and Privacy Policy.'
+    },
+    {
+        color:'blue',
+        number :' 2.9.3 (02/05/2020)',
+        button:'Fix',
+        text :'Google Drive or backup to Google Photos, and access all of your content directly from your PC or Mac.Go to Google Drive Download Drive for desktop.'
+    },
+    {
+        color:'purple',
+        number :'2.2.0 (01/11/2019)' ,
+        button:'Improvement',
+        text: 'You can download a copy of the data saved  in your Google Account to use it with another account or service, or to keep for your records.'  
+    },
+    {
+        color:'green',
+        number:  '3.3.0 (12/12/2021)' ,
+        button:'New',
+        text:'Mac or Windows PC. WhatsApp must be installed on your phone. By clicking the Download button, you agree to our Terms and Privacy Policy.'
+    },
+    {
+        color:'blue',
+        number :' 2.9.3 (02/05/2020)',
+        button:'Fix',
+        text :'Google Drive or backup to Google Photos, and access all of your content directly from your PC or Mac.Go to Google Drive Download Drive for desktop.'
+    },
+    {
+        color:'purple',
+        number :'2.2.0 (01/11/2019)' ,
+        button:'Improvement',
+        text: 'You can download a copy of the data saved  in your Google Account to use it with another account or service, or to keep for your records.'  
+    }
+         
+]
+
